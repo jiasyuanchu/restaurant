596029741e4a4340c7dd2f485900500f53a5a7a9
jiasyuanchu <jiasyuanchu@gmail.com>
2023-03-29 14:48:50 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,12 +1,32 @@
 // app.js
 // require packages used in the project
 const express = require('express')
-const app = express()
-const port = 3000
-
 // require handlebars in the project
 const exphbs = require('express-handlebars')
 const restaurantList = require('./restaurant.json')
+const mongoose = require('mongoose') // 載入 mongoose
+
+// 僅在非正式環境時使用 dotenv
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+const app = express()
+const port = 3000
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }) // 設定連線到 mongoDB
+
+
+// 取得資料庫連線狀態
+const db = mongoose.connection
+// 連線異常
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+// 連線成功
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
