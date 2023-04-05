9763477c8f22678e68d7d258c0a899ace1c807ad
jiasyuanchu <jiasyuanchu@gmail.com>
2023-04-05 15:00:31 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -1,50 +1,22 @@
 // app.js
 // require packages used in the project
 const express = require('express')
-const mongoose = require('mongoose') 
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
+
 const routes = require('./routes')
+require('./config/mongoose')
 
 const restaurantList = require('./restaurant.json')
-
-// 僅在非正式環境時使用 dotenv
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
-}
 
 const app = express()
 const port = 3000
 
-console.log('process.env.NODE_ENV', process.env.NODE_ENV)
-if (process.env.NODE_ENV !== 'production') {
-  console.log('here')
-  require('dotenv').config()
-}
-
-
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }) // 設定連線到 mongoDB
-
-
-// 取得資料庫連線狀態
-const db = mongoose.connection
-// 連線異常
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-// 連線成功
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
-
-
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
-
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
-
 // setting static files
 app.use(express.static('public'))
 app.use(routes) // 將 request 導入路由器
