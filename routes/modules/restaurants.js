c2291f5001d7f31ebcbd7532b33e180dba9ef40a
jiasyuanchu <jiasyuanchu@gmail.com>
2023-04-07 14:58:55 +0800

    fix: router fine tuning

--- routes/modules/restaurants.js
+++ routes/modules/restaurants.js
@@ -12,9 +12,11 @@ router.get('/restaurants/new', (req, res) => {
 // receive the results and transfer results to database
 router.post('/restaurants', (req, res) => {
   Restaurant.create(req.body)
-    .then(() => res.redirect('/'))
+    .then(thisNewRestaurant => {
+      res.render('detail', { thisNewRestaurant });
+    })
     .catch(err => console.log(err))
-})
+    });
 
 // direct to specific details
 router.get('/restaurants/:id', (req, res) => {
@@ -36,28 +38,34 @@ router.get('/restaurants/:id/edit', (req, res) => {
 
 // receive editing results and transfer results to database
 router.put('/restaurants/:id', (req, res) => {
-  const id = req.params.id
-  const data = req.body
-  // modify restaurant, and save to data
-  return Restaurant.findById(id)
-    .then(restaurant => {
-      restaurant.name = data.name,
-      restaurant.name_en = data.name_en,
-      restaurant.category = data.category,
-      restaurant.image = data.image,
-      restaurant.location = data.location,
-      restaurant.phone = data.phone,
-      restaurant.google_map = data.google_map,
-      restaurant.rating = data.rating,
-      restaurant.description = data.description,
-      restaurant.save()
-    })
-    .then(() => {
-      res.redirect(`/restaurants/${id}/`)
-    })
-    .catch(error => {
-      console.log(error)
-    })
+  //use the object assign 
+  const { id } = req.params
+  Restaurant.findByIdAndUpdate(id, req.body)
+    .then(() => res.redirect(`/restaurants/${id}`))
+    .catch(err => console.log(err))
+
+  // const id = req.params.id
+  // const data = req.body
+  // // modify restaurant, and save to data
+  // return Restaurant.findById(id)
+  //   .then(restaurant => {
+  //     restaurant.name = data.name,
+  //     restaurant.name_en = data.name_en,
+  //     restaurant.category = data.category,
+  //     restaurant.image = data.image,
+  //     restaurant.location = data.location,
+  //     restaurant.phone = data.phone,
+  //     restaurant.google_map = data.google_map,
+  //     restaurant.rating = data.rating,
+  //     restaurant.description = data.description,
+  //     restaurant.save()
+  //   })
+  //   .then(() => {
+  //     res.redirect(`/restaurants/${id}/`)
+  //   })
+  //   .catch(error => {
+  //     console.log(error)
+  //   })
 })
 
 // delete function
@@ -70,8 +78,12 @@ router.delete('/restaurants/:id', (req, res) => {
 })
 
 router.get('/restaurants/:restaurant_id', (req, res) => {
-  const restaurant = restaurantList.results.find(restaurant => restaurant.id.toString() === req.params.restaurant_id)
-  res.render('show', { restaurant })
+  const { restaurant_id } = req.params
+  //search in "restaurant"(in db) by ID
+  Restaurant.findById(restaurant_id)
+    .lean()
+    .then(restaurant => res.render("show", { restaurant }))
+    .catch(err => console.log(err))
 })
 
 // 匯出路由模組
