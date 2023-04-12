bc6ab87f05337edc42dbacdaebad8a840d97bf20
jiasyuanchu <jiasyuanchu@gmail.com>
2023-04-12 17:08:30 +0800

    fix: add search. js but still fail.

---
+++ routes/modules/search.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const router = express.Router()
+const Restaurant = require('../../models/restaurant')
+
+
+// search
+router.get('/', (req, res) => {
+  if (!req.query.keyword) {
+    return res.redirect('/')
+  }
+  else {
+    console.log(req.query)
+    const keyword = req.query.keyword.trim().toLowerCase()
+    const sortMethod = req.query.sort
+    let sortKeyword = { name: 'asc' }
+    if (sortMethod === 'Z > A')
+      sortKeyword = { name: 'desc' }
+    if (sortMethod === '類別')
+      sortKeyword = { category: 'asc' }
+    if (sortMethod === '地區')
+      sortKeyword = { location: 'asc' }
+  }
+  return Restaurant.find({})
+    .lean()
+    .sort(sortKeyword)
+    .then(restaurantsData => {
+      const results = restaurantsData.filter(
+        data =>
+          data.name.toLowerCase().includes(keyword) ||
+          data.category.includes(keyword) ||
+          data.name_en.toLowerCase().includes(keyword)
+      )
+      res.render('index', { restaurantsData: results, keyword, sortMethod })
+    })
+    .catch(err => {
+      console.log(err)
+      res.render('errorPage', { error: err.message })
+    })
+})
+
+module.exports = router
