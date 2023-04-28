4ee720e775481ae9808b4ff9b6ba7bd1e9a47c19
jiasyuanchu <jiasyuanchu@gmail.com>
2023-04-29 01:21:22 +0800

    fix: restaurant sorting function fixed.

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -13,29 +13,5 @@ router.get('/', (req, res) => {
     .catch(error => console.error(error))
 })
 
-// 搜尋功能
-router.get('/search', (req, res) => {
-  const sort = req.query.sort;
-  console.log(sort);
-  const keyword = req.query.keyword.trim().toLowerCase()
-  Restaurant.find()
-    .lean()
-    .then(restaurants => {
-      const filteredRestaurants = restaurants.filter(
-        (restaurant) =>
-          restaurant.name.toLowerCase().includes(keyword) ||
-          restaurant.category.includes(keyword)
-      )
-
-      if (filteredRestaurants.length) {
-        // 如果有搜尋結果，執行以下
-        res.render('index', { restaurants: filteredRestaurants, keyword })
-      } else {
-        res.render('index', { keyword, errorMsg: `您搜尋的關鍵字：${keyword}，沒有符合條件的餐廳` })
-      }
-    })
-    .catch(error => console.error(error))
-})
-
 // 匯出路由模組
 module.exports = router
