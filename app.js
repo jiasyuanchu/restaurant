343c1f3771cbbd11bff67dd593b73159a51b25df
jiasyuanchu <jiasyuanchu@gmail.com>
2023-04-29 20:41:17 +0800

    feat: add isAuthenticated to display different view templates

--- app.js
+++ app.js
@@ -41,6 +41,13 @@ app.use(express.static('public'))
 
 usePassport(app)// 呼叫 Passport 函式並傳入 app，這條要寫在路由之前
 
+app.use((req, res, next) => {
+  console.log(req.user)
+  res.locals.isAuthenticated = req.isAuthenticated()
+  res.locals.user = req.user
+  next()
+})
+
 app.use(routes) // 將 request 導入路由器
 
 // start and listen on the Express server
